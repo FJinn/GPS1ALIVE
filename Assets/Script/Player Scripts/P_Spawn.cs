4f4d662aea549c278807d0a59e354815531d520f
QUAN-PC\QUAN <quanpro777@hotmail.com>
2018-06-29 13:18:22 +0800

    Updated: Added checkpoint functions. Deleted unnecessary tiles.

---
+++ Assets/Script/Player Scripts/P_Spawn.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class P_Spawn : MonoBehaviour {
+
+    public GameObject spawnLocation;
+    private GameObject spawnManager;
+
+	// Use this for initialization
+	void Start () {
+        spawnManager = GameObject.Find("EO_CheckpointManager");
+        if (gameObject.CompareTag("Player"))
+        {
+            transform.position = spawnManager.GetComponent<Checkpoint>().spawnPoint + new Vector3(-5, 0,0);
+        }else
+        if(gameObject.CompareTag("Player2"))
+        {
+            transform.position = spawnManager.GetComponent<Checkpoint>().spawnPoint + new Vector3(3, 0, 0);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
