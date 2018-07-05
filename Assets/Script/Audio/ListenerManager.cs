ed7469fdcae0e7aea19fe5e22a792d561ad723c1
FJinn <0123433@kdu-online.com>
2018-07-05 23:30:32 +0800

    Added volume slider for sfx and bgm. Use ambient light as brightness for now (still not sure about this). Control scheme in main menu will lead to setting canvas. Added back button in setting canvas to back to main menu.

---
+++ Assets/Script/Audio/ListenerManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ListenerManager : MonoBehaviour {
+
+    // this will let user set BGM volume
+    private float userSetVolume;
+    // to get the slider object
+    public GameObject slider;
+
+    void Update()
+    {
+        // so volume = user-set-slider value
+        userSetVolume = slider.GetComponent<Slider>().value;
+
+        // set audio volume to userSetVolume
+        AudioListener.volume = userSetVolume;
+
+        DontDestroyOnLoad(this.gameObject);
+    }
+}
