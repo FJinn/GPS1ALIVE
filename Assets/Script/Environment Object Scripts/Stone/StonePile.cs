bb12423614df62e2086308f1453b241cf06b718a
FJinn <0123433@kdu-online.com>
2018-07-11 22:00:59 +0800

    Stone pile code.

---
+++ Assets/Script/Environment Object Scripts/Stone/StonePile.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StonePile : MonoBehaviour {
+
+    private GameObject[] p = new GameObject[2];
+
+    // Use this for initialization
+    void Start () {
+        p[0] = GameObject.FindGameObjectWithTag("Player");
+        p[1] = GameObject.FindGameObjectWithTag("Player2");
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        for (int i = 0; i < p.Length; i++)
+        {
+            if (Input.GetKeyDown(p[i].GetComponent<P_controls>().KeyUse) && p[i].GetComponent<P_throw>().spawnStone == 0)
+            {
+                p[i].GetComponent<P_throw>().spawnStone = 1;
+            }
+        }
+    }
+}
