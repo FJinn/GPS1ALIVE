ecb801541017caf996412fea7c8c4955aa47aa4b
FJinn <0123433@kdu-online.com>
2018-07-22 13:13:28 +0800

    Added editable sound radius for stone in Stone Pile Object.

--- Assets/Script/Environment Object Scripts/Stone/StonePile.cs
+++ Assets/Script/Environment Object Scripts/Stone/StonePile.cs
@@ -7,6 +7,8 @@ public class StonePile : MonoBehaviour {
     private GameObject[] p = new GameObject[2];
     [Header("Adjust stone projectile height")] //Changing speed Y to adjust stone projectile height
     public float height;
+    [Header("Change the spawning stone sound radius")]
+    public float soundRadius;
 
     // Use this for initialization
     void Start () {
@@ -21,7 +23,11 @@ public class StonePile : MonoBehaviour {
             if (Input.GetKeyDown(p[i].GetComponent<P_controls>().KeyUse) && p[i].GetComponent<P_throw>().throwStance == false)
             {
                 p[i].GetComponent<P_throw>().spawnStone = 1;
+            }
 
+            if (p[i].GetComponent<P_throw>().stoneTemp != null)
+            {
+                p[i].GetComponent<P_throw>().stoneTemp.GetComponent<S_SoundRadius>().s_soundRadius = soundRadius;
             }
 
             p[i].GetComponent<P_throw>().speedY = height;
