ef25eaa5fad80dd9d55370f7f55b79365eb42e4a
FJinn <0123433@kdu-online.com>
2018-07-22 16:09:38 +0800

    Stone will no collide with player and other stone.

--- Assets/Script/Player Scripts/P_throw.cs
+++ Assets/Script/Player Scripts/P_throw.cs
@@ -20,6 +20,9 @@ public class P_throw : MonoBehaviour {
     public float s_indicatorHeight;
     public float s_indicatorWidth;
 
+    GameObject[] player = new GameObject[2];
+    GameObject otherStone;
+
     private P_controls control;
 
     void Start()
@@ -27,6 +30,9 @@ public class P_throw : MonoBehaviour {
         control = GetComponent<P_controls>();
         // Offset Y
         tempYSize = GetComponent<BoxCollider2D>().size.y / 2;
+
+        player[0] = GameObject.FindGameObjectWithTag("Player");
+        player[1] = GameObject.FindGameObjectWithTag("Player2");
     }
 
     // Update is called once per frame
@@ -91,7 +97,11 @@ public class P_throw : MonoBehaviour {
 	void throwing(){
 		stoneTemp = (GameObject)Instantiate (stone, tempPos, Quaternion.identity);
         // ignore collision with stone
-        Physics2D.IgnoreCollision(this.GetComponent<BoxCollider2D>(), stoneTemp.GetComponent<BoxCollider2D>());
+        Physics2D.IgnoreCollision(player[0].GetComponent<BoxCollider2D>(), stoneTemp.GetComponent<BoxCollider2D>());
+        Physics2D.IgnoreCollision(player[1].GetComponent<BoxCollider2D>(), stoneTemp.GetComponent<BoxCollider2D>());
+
+        otherStone = GameObject.FindGameObjectWithTag("Stone");
+        Physics2D.IgnoreCollision(otherStone.GetComponent<BoxCollider2D>(), stoneTemp.GetComponent<BoxCollider2D>());
 
         spawnStone = 0;
         isThrowing = true;
