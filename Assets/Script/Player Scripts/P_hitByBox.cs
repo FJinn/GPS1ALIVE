d8e688dfd5d0a82c70d6ccbfb980f642bce5ee57
FJinn <0123433@kdu-online.com>
2018-07-29 15:25:10 +0800

    Player head box collider will disable when climibing ladder

--- Assets/Script/Player Scripts/P_hitByBox.cs
+++ Assets/Script/Player Scripts/P_hitByBox.cs
@@ -5,10 +5,30 @@ using UnityEngine;
 public class P_hitByBox : MonoBehaviour {
 
     GameObject temp;
-
+    P_controls pControls;
+    BoxCollider2D bc2d;
+    
     private void Start()
     {
         temp = transform.parent.gameObject;
+        bc2d = GetComponent<BoxCollider2D>();
+    }
+
+    private void Update()
+    {
+        if(pControls == null)
+        {
+            pControls = GetComponentInParent<P_controls>();
+        }
+
+        if (pControls.OnLadder)
+        {
+            bc2d.enabled = false;
+        }
+        else
+        {
+            bc2d.enabled = true;
+        }
     }
 
     private void OnTriggerEnter2D (Collider2D other)
