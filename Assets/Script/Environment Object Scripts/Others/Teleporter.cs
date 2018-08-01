b3c8a8e3487550fb652ba66e659f6c2424453753
QUAN-PC\QUAN <quanpro777@hotmail.com>
2018-08-01 21:18:24 +0800

    Fixed a lot weird bugs that I don't even remember.

---
+++ Assets/Script/Environment Object Scripts/Others/Teleporter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleporter : MonoBehaviour {
+
+    [SerializeField] GameObject teleportPos1;
+    [SerializeField] GameObject teleportPos2;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            collision.transform.position = teleportPos1.transform.position;
+        }
+
+        if (collision.CompareTag("Player2"))
+        {
+            collision.transform.position = teleportPos2.transform.position;
+        }
+    }
+}
