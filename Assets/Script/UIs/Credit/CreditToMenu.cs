b47d6c4ccc175ec26656db6458a01fc7fa4ede3d
zjLee <0118259@kdu-online.com>
2018-08-05 18:02:40 +0800

    Finished Credits

---
+++ Assets/Script/UIs/Credit/CreditToMenu.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditToMenu : MonoBehaviour {
+
+    public GameObject lvlChanger;
+
+	public void toMenu()
+    {
+        lvlChanger.GetComponent<LevelChanger>().FadeToLevel(1);
+    }
+}
